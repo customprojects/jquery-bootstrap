a6f02f1cd428d06cde9d43ef6587b512db551ea6
arielhm888 <arielhm@inca.edu.cu>
2013-02-18 08:59:35 -0800

    updating my plugin

--- README.md
+++ README.md
@@ -9,24 +9,21 @@ Installation
 grails insttall-plugin jquery-bootstrap
 Usage
 ===============
-This plugin provide a tag lib class with 2 tags: cssResources and jsResources<br>
-cssResources: Load all needed styles for jquery-ui and bootstrap.<br>
-jsResources: Load all needed js files.<br>
+This plugin provide a tag lib class with 1 tags: resources<br>
+resources: Load all needed styles and js files for jquery-ui and bootstrap.<br>
 <br>
 
 Make sure your layout be as follows<br>
 ```html
-<html>
+<!DOCTYPE html>
     <head>
-    <!-- ... -->
+    <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1"/>
     <title><g:layoutTitle default="Grails"/></title><br>
 		<!-- ... -->
-    <jb:cssResources/>
+		<jb:resources/>
 	</head>
 	<body>
-    <!-- ... -->
-	  <jb:jsResources/>
-    <!-- your js code here -->
+		<!-- ... -->
 	</body>
 </html>
 ```
--- grails-app/taglib/grails/plugin/jquerybootstrap/JQueryBootStrapTagLib.groovy
+++ grails-app/taglib/grails/plugin/jquerybootstrap/JQueryBootStrapTagLib.groovy
@@ -1,17 +1,49 @@
 package grails.plugin.jquerybootstrap
 
-class JQueryBootStrapTagLib {
-	static namespace = "jb"
+class JQueryBootstrapTagLib {
 	
-	def cssResources = {
-		out << "<link href=\"${g.resource(dir: 'bootstrap/css', file: 'bootstrap.css', plugin: 'jquery-bootstrap')}\" rel=\"stylesheet\">\n"
-		out << "<link href=\"${g.resource(dir: 'bootstrap/css', file: 'bootstrap-responsive.css', plugin: 'jquery-bootstrap')}\" rel=\"stylesheet\">\n"
+	static namespace = 'jb'
+	
+	def resources = {
+		out << "<link href=\"${g.resource(dir: 'bootstrap/css', file: 'bootstrap.min.css', plugin: 'jquery-bootstrap')}\" rel=\"stylesheet\">\n"
+		out << "<link href=\"${g.resource(dir: 'bootstrap/css', file: 'bootstrap-responsive.min.css', plugin: 'jquery-bootstrap')}\" rel=\"stylesheet\">\n"
 		out << "<link href=\"${g.resource(dir: 'css', file: 'jquery-ui-1.9.2.custom.css', plugin: 'jquery-bootstrap')}\" rel=\"stylesheet\">\n"
+		out << "<script src=\"${g.resource(dir: 'js', file: 'jquery-1.9.1.min.js', plugin: 'jquery-bootstrap')}\"></script>\n"
+		out << "<script src=\"${g.resource(dir: 'bootstrap/js', file: 'bootstrap.min.js', plugin: 'jquery-bootstrap')}\"></script>\n"
+		out << "<script src=\"${g.resource(dir: 'bootstrap/js', file: 'jquery-ui-1.9.2.custom.min.js', plugin: 'jquery-bootstrap')}\"></script>\n"
 	}
 	
-	def jsResources = {
-		out << "<script src=\"${g.resource(dir: 'js', file: 'jquery-1.9.1.min.js', plugin: 'jquery-bootstrap')}\"></script>\n"
-		out << "<script src=\"${g.resource(dir: 'js', file: 'jquery-ui-1.9.2.custom.min.js', plugin: 'jquery-bootstrap')}\"></script>\n"
-		out << "<script src=\"${g.resource(dir: 'bootstrap/js', file: 'bootstrap.js', plugin: 'jquery-bootstrap')}\"></script>\n"
+	def btn = {attrs->
+		out << "<input type=\"button\" class=\"btn\" value=\"${attrs.label}\" id=\"${attrs.id}\">"
+	}
+	
+	def btnPrimary = {attrs->
+		out << "<input type=\"button\" class=\"btn btn-primary\" value=\"${attrs.label}\" id=\"${attrs.id}\">"
+	}
+		
+	def btnSuccess = {attrs->
+		out << "<input type=\"button\" class=\"btn btn-success\" value=\"${attrs.label}\" id=\"${attrs.id}\">"
+	}
+	
+	def btnDanger = {attrs->
+		out << "<input type=\"button\" class=\"btn btn-danger\" value=\"${attrs.label}\" id=\"${attrs.id}\">"
+	}
+	
+	def dialog = {attrs, body->
+		out << "<div id=\"${attrs.id}\" title=\"${attrs.title}\">"
+		out << body()
+		out << "</div>"
+		out << """
+			<script>
+				\$(function(){
+					\$('#${attrs.id}').dialog({
+						autoOpen: ${!attrs.autoOpen?'false':'true'}
+						${!attrs.modal?',modal: false':',modal:'+attrs.modal}
+						${!attrs.width?'':',width: '+attrs.width}
+						${!attrs.height?'':',height: '+attrs.height}
+					});
+				});
+			</script>
+"""
 	}
 }
