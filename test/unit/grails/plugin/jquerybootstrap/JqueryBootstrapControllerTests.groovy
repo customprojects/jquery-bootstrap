80f017bc5460fcf06293ab133a67b68cb0d5dc01
arielhm888 <arielhm@inca.edu.cu>
2013-02-18 09:06:54 -0800

    Adding a controller to test

---
+++ test/unit/grails/plugin/jquerybootstrap/JqueryBootstrapControllerTests.groovy
@@ -0,0 +1,17 @@
+package grails.plugin.jquerybootstrap
+
+
+
+import grails.test.mixin.*
+import org.junit.*
+
+/**
+ * See the API for {@link grails.test.mixin.web.ControllerUnitTestMixin} for usage instructions
+ */
+@TestFor(JqueryBootstrapController)
+class JqueryBootstrapControllerTests {
+
+    void testSomething() {
+       fail "Implement me"
+    }
+}
